a78902f157e635e4cbd433800e1e4f035b3a24d3
Reece Lim <111989096+NotRelix@users.noreply.github.com>
2025-05-02 22:57:24 +0800

    feat: add placeShip() to Gameboard

--- src/classes/gameboard.js
+++ src/classes/gameboard.js
@@ -1,3 +1,5 @@
+import { Ship } from "./ship";
+
 export class Gameboard {
   constructor() {
     this.board = this.generateBoard();
@@ -15,4 +17,49 @@ export class Gameboard {
     }
     return board;
   }
+
+  placeShip(x, y, length, isVertical) {
+    if (!isValidShipPlacement(this.board, x, y, length, isVertical)) {
+      return;
+    }
+
+    const ship = new Ship(length);
+    const currentShipIndex = this.ships.length;
+    this.ships.push(ship);
+    for (let i = 0; i < length; i++) {
+      isVertical
+        ? (this.board[x + i][y] = currentShipIndex)
+        : (this.board[x][y + i] = currentShipIndex);
+    }
+  }
+}
+
+function isValidShipPlacement(board, x, y, length, isVertical) {
+  for (let i = 0; i < length; i++) {
+    const currentX = isVertical ? x + i : x;
+    const currentY = isVertical ? y : y + i;
+    const surroundingCoordinates = [
+      [currentX - 1, currentY - 1],
+      [currentX - 1, currentY],
+      [currentX - 1, currentY + 1],
+      [currentX, currentY - 1],
+      [currentX, currentY],
+      [currentX, currentY + 1],
+      [currentX + 1, currentY - 1],
+      [currentX + 1, currentY],
+      [currentX + 1, currentY + 1],
+    ];
+    if (currentX < 0 || currentX > 9 || currentY < 0 || currentY > 9) {
+      return false;
+    }
+    for (let coords of surroundingCoordinates) {
+      if (coords[0] < 0 || coords[0] > 9 || coords[1] < 0 || coords[1] > 9) {
+        continue;
+      }
+      if (board[coords[0]][coords[1]] !== null) {
+        return false;
+      }
+    }
+  }
+  return true;
 }
--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -1,5 +1,180 @@
+import { Gameboard } from "../classes/gameboard";
+
 describe("Gameboard Class", () => {
-  test("Place One Ship", () => {
-    // TODO: Make placeShip()
+  const gameboard = new Gameboard();
+  test("Place One Vertical Ship", () => {
+    gameboard.placeShip(0, 0, 5, true);
+    expect(gameboard.board).toStrictEqual([
+      [0, null, null, null, null, null, null, null, null, null],
+      [0, null, null, null, null, null, null, null, null, null],
+      [0, null, null, null, null, null, null, null, null, null],
+      [0, null, null, null, null, null, null, null, null, null],
+      [0, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+    ]);
+  });
+
+  test("Place One Horizontal Ship", () => {
+    gameboard.placeShip(9, 1, 3, false);
+    expect(gameboard.board).toStrictEqual([
+      [0, null, null, null, null, null, null, null, null, null],
+      [0, null, null, null, null, null, null, null, null, null],
+      [0, null, null, null, null, null, null, null, null, null],
+      [0, null, null, null, null, null, null, null, null, null],
+      [0, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, 1, 1, 1, null, null, null, null, null, null],
+    ]);
+  });
+
+  test("Can't Place a Colliding Ship Horizontally", () => {
+    gameboard.placeShip(2, 0, 5, false);
+    expect(gameboard.board).toStrictEqual([
+      [0, null, null, null, null, null, null, null, null, null],
+      [0, null, null, null, null, null, null, null, null, null],
+      [0, null, null, null, null, null, null, null, null, null],
+      [0, null, null, null, null, null, null, null, null, null],
+      [0, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, 1, 1, 1, null, null, null, null, null, null],
+    ]);
+  });
+
+  test("Can't Place Next to a Ship Horizontally", () => {
+    gameboard.placeShip(1, 1, 5, false);
+    expect(gameboard.board).toStrictEqual([
+      [0, null, null, null, null, null, null, null, null, null],
+      [0, null, null, null, null, null, null, null, null, null],
+      [0, null, null, null, null, null, null, null, null, null],
+      [0, null, null, null, null, null, null, null, null, null],
+      [0, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, 1, 1, 1, null, null, null, null, null, null],
+    ]);
+  });
+
+  test("Place Vertical Ship Normally", () => {
+    gameboard.placeShip(1, 2, 5, true);
+    expect(gameboard.board).toStrictEqual([
+      [0, null, null, null, null, null, null, null, null, null],
+      [0, null, 2, null, null, null, null, null, null, null],
+      [0, null, 2, null, null, null, null, null, null, null],
+      [0, null, 2, null, null, null, null, null, null, null],
+      [0, null, 2, null, null, null, null, null, null, null],
+      [null, null, 2, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, 1, 1, 1, null, null, null, null, null, null],
+    ]);
+  });
+
+  test("Place Vertical Ship Normally But Too Long", () => {
+    gameboard.placeShip(6, 5, 5, true);
+    expect(gameboard.board).toStrictEqual([
+      [0, null, null, null, null, null, null, null, null, null],
+      [0, null, 2, null, null, null, null, null, null, null],
+      [0, null, 2, null, null, null, null, null, null, null],
+      [0, null, 2, null, null, null, null, null, null, null],
+      [0, null, 2, null, null, null, null, null, null, null],
+      [null, null, 2, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, 1, 1, 1, null, null, null, null, null, null],
+    ]);
+  });
+
+  test("Place Vertical Ship Normally But Just Right", () => {
+    gameboard.placeShip(6, 5, 4, true);
+    expect(gameboard.board).toStrictEqual([
+      [0, null, null, null, null, null, null, null, null, null],
+      [0, null, 2, null, null, null, null, null, null, null],
+      [0, null, 2, null, null, null, null, null, null, null],
+      [0, null, 2, null, null, null, null, null, null, null],
+      [0, null, 2, null, null, null, null, null, null, null],
+      [null, null, 2, null, null, null, null, null, null, null],
+      [null, null, null, null, null, 3, null, null, null, null],
+      [null, null, null, null, null, 3, null, null, null, null],
+      [null, null, null, null, null, 3, null, null, null, null],
+      [null, 1, 1, 1, null, 3, null, null, null, null],
+    ]);
+  });
+
+  test("Place Horizontal Ship But Just Right", () => {
+    gameboard.placeShip(0, 8, 2, false);
+    expect(gameboard.board).toStrictEqual([
+      [0, null, null, null, null, null, null, null, 4, 4],
+      [0, null, 2, null, null, null, null, null, null, null],
+      [0, null, 2, null, null, null, null, null, null, null],
+      [0, null, 2, null, null, null, null, null, null, null],
+      [0, null, 2, null, null, null, null, null, null, null],
+      [null, null, 2, null, null, null, null, null, null, null],
+      [null, null, null, null, null, 3, null, null, null, null],
+      [null, null, null, null, null, 3, null, null, null, null],
+      [null, null, null, null, null, 3, null, null, null, null],
+      [null, 1, 1, 1, null, 3, null, null, null, null],
+    ]);
+  });
+
+  test("Place Horizontal Ship But Too Long", () => {
+    gameboard.placeShip(3, 6, 5, false);
+    expect(gameboard.board).toStrictEqual([
+      [0, null, null, null, null, null, null, null, 4, 4],
+      [0, null, 2, null, null, null, null, null, null, null],
+      [0, null, 2, null, null, null, null, null, null, null],
+      [0, null, 2, null, null, null, null, null, null, null],
+      [0, null, 2, null, null, null, null, null, null, null],
+      [null, null, 2, null, null, null, null, null, null, null],
+      [null, null, null, null, null, 3, null, null, null, null],
+      [null, null, null, null, null, 3, null, null, null, null],
+      [null, null, null, null, null, 3, null, null, null, null],
+      [null, 1, 1, 1, null, 3, null, null, null, null],
+    ]);
+  });
+
+  test("Place Vertical Ship On Top of Another", () => {
+    gameboard.placeShip(3, 5, 3, true);
+    expect(gameboard.board).toStrictEqual([
+      [0, null, null, null, null, null, null, null, 4, 4],
+      [0, null, 2, null, null, null, null, null, null, null],
+      [0, null, 2, null, null, null, null, null, null, null],
+      [0, null, 2, null, null, null, null, null, null, null],
+      [0, null, 2, null, null, null, null, null, null, null],
+      [null, null, 2, null, null, null, null, null, null, null],
+      [null, null, null, null, null, 3, null, null, null, null],
+      [null, null, null, null, null, 3, null, null, null, null],
+      [null, null, null, null, null, 3, null, null, null, null],
+      [null, 1, 1, 1, null, 3, null, null, null, null],
+    ]);
+  });
+
+  test("Place Horizontal Ship Next to Another", () => {
+    gameboard.placeShip(0, 5, 3, false);
+    expect(gameboard.board).toStrictEqual([
+      [0, null, null, null, null, null, null, null, 4, 4],
+      [0, null, 2, null, null, null, null, null, null, null],
+      [0, null, 2, null, null, null, null, null, null, null],
+      [0, null, 2, null, null, null, null, null, null, null],
+      [0, null, 2, null, null, null, null, null, null, null],
+      [null, null, 2, null, null, null, null, null, null, null],
+      [null, null, null, null, null, 3, null, null, null, null],
+      [null, null, null, null, null, 3, null, null, null, null],
+      [null, null, null, null, null, 3, null, null, null, null],
+      [null, 1, 1, 1, null, 3, null, null, null, null],
+    ]);
   });
 });
