0fd245cbb466649c3a1397395b7a1adecbc15d35
Reece Lim <111989096+NotRelix@users.noreply.github.com>
2025-05-12 14:37:01 +0800

    feat: make computer move after player's turn

--- src/modules/game.js
+++ src/modules/game.js
@@ -1,8 +1,23 @@
 import { attackCell } from "./screen";
 
+function makeComputerMove(currentPlayer) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const randX = Math.floor(Math.random() * 10);
+      const randY = Math.floor(Math.random() * 10);
+      const currentCell = document.querySelector(
+        `[data-x="${randX}"][data-y="${randY}"]`,
+      );
+      const isSuccessfulAttack = attackCell(currentPlayer, currentCell);
+      console.log(isSuccessfulAttack);
+      resolve(true);
+    }, 2000);
+  });
+}
+
 export function game(playerOne, playerTwo) {
-  let currentPlayer = playerOne;
-  document.addEventListener("click", (e) => {
+  let currentPlayer = playerTwo;
+  document.addEventListener("click", async (e) => {
     if (
       !e.target.closest(`.${currentPlayer.kebabName}`) ||
       !e.target.classList.contains("col") ||
@@ -11,10 +26,15 @@ export function game(playerOne, playerTwo) {
     ) {
       return;
     }
+    if (currentPlayer === playerOne) {
+      return;
+    }
     const currentCell = e.target;
     const isSuccessfulAttack = attackCell(currentPlayer, currentCell);
     if (!isSuccessfulAttack) {
-      currentPlayer = currentPlayer === playerOne ? playerTwo : playerOne;
+      currentPlayer = playerOne;
+      await makeComputerMove(currentPlayer);
+      currentPlayer = playerTwo;
     }
   });
 }
